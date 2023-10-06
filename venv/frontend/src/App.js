0f9e4505f40035b79e419d095c48deb1aa997d12
Ada <adabingw@yahoo.com>
2023-10-05 20:49:25 -0400

    :sake:

--- venv/frontend/src/App.js
+++ venv/frontend/src/App.js
@@ -48,7 +48,7 @@ function App() {
         <div className="title">
             LYRR
         </div>
-        <a href="https://github.com/adabingw/lyrr" target="_blank"><i class="fa-brands fa-github fa-sm"></i></a>
+        <a href="https://github.com/adabingw/lyrr" target="_blank"><i class="fa-brands fa-github fa-lg"></i></a>
         enter artist name: 
         <input type="text" onChange={(e) => setArtist(e.target.value)} className="input" 
             onKeyDown={(e) => handlePress(e)}
